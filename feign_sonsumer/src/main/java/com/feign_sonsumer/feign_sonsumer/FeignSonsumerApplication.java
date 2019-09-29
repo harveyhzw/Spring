c45ea59c0734c728d389adaff7066f640c16e7b6
harveyhzw <591604023@qq.com>
2019-09-29 08:43:28 +0800

    Add demo for hystrix dashborad and turbine

---
+++ feign_sonsumer/src/main/java/com/feign_sonsumer/feign_sonsumer/FeignSonsumerApplication.java
@@ -0,0 +1,13 @@
+package com.feign_sonsumer.feign_sonsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignSonsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignSonsumerApplication.class, args);
+    }
+
+}
